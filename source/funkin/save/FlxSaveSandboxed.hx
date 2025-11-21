1b72b457b529d3106f809f6c40aac0cf29871cb9
ComedyLost <106041578+ComedyLost@users.noreply.github.com>
2025-11-20 21:16:55 -0500

    use constants instead

--- source/funkin/save/FlxSaveSandboxed.hx
+++ source/funkin/save/FlxSaveSandboxed.hx
@@ -11,10 +11,6 @@ import haxe.Exception;
 @:nullSafety
 class FlxSaveSandboxed extends FlxSave
 {
-  static final SAVE_PATH:String = 'FunkinCrew';
-  static final SAVE_NAME:String = 'Funkin';
-  static final BASE_SAVE_SLOT:Int = 1;
-
   public function new()
   {
     super();
@@ -22,20 +18,20 @@ class FlxSaveSandboxed extends FlxSave
   
 	override public function bind(name:String, ?path:String, ?backupParser:(String, Exception)->Null<Any>):Bool
   {
-    if (name == '$SAVE_NAME${BASE_SAVE_SLOT}' && path == null) throw 'Unable to bind to $name. Use funkin.save.Save instead.';
-    return super.bind(name, '$SAVE_PATH${path != null ? '/$path' : ''}', backupParser);
+    if (name == Constants.SAVE_NAME + Constants.BASE_SAVE_SLOT && path == null) throw 'Unable to bind to $name. Use funkin.save.Save instead.';
+    return super.bind(name, Constants.SAVE_PATH + (path != null ? '/$path' : ''), backupParser);
   }
 
   override public function mergeDataFrom(name:String, ?path:String, overwrite = false, eraseSave = true, minFileSize = 0):Bool
   {
-    if (name == '$SAVE_NAME${BASE_SAVE_SLOT}' && path == null) throw 'Unable to merge save. Requested save data cannot be a Funkin save.';
-    return super.mergeDataFrom(name, '$SAVE_PATH${path != null ? '/$path' : ''}', overwrite, false, minFileSize);
+    if (name == Constants.SAVE_NAME + Constants.BASE_SAVE_SLOT && path == null) throw 'Unable to merge from a Funkin save.';
+    return super.mergeDataFrom(name, Constants.SAVE_PATH + (path != null ? '/$path' : ''), overwrite, false, minFileSize);
   }  
 
   @:unreflective
   override public function erase():Bool
   {
-    if (this.name == '$SAVE_NAME${BASE_SAVE_SLOT}' && this.path == SAVE_PATH) throw 'Unable to delete ${this.name} because it\'s a Funkin save.';
+    if (this.name == Constants.SAVE_NAME + Constants.BASE_SAVE_SLOT && this.path == Constants.SAVE_PATH) throw 'Unable to delete ${this.name}.';
     return super.erase();
   }  
 }
